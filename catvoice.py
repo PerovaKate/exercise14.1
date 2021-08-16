ece6e0f425473d62f3bf5c48854e6fb400e75323
Vandalism <anotherboxforspam@gmail.com>
2021-08-16 19:20:01 +0300

    getvoice

--- catvoice.py
+++ catvoice.py
@@ -1,19 +1,30 @@
-def cat_voice ():
-   print('meow!')
-cat_voice()
+# def cat_voice ():
+#    print('meow!')
+# cat_voice()
+#
+# def dog_voice ():
+#     print('woof!')
+# dog_voice()
+#
+# #Вывод голосов N раз
+#
+# def cat_voice ():
+#     return ('meow!')
+# for x in range(2):
+#     print(cat_voice())
+#
+# def dog_voice ():
+#     return ('woof!')
+# for x in range(2):
+#     print(dog_voice())
 
-def dog_voice ():
-    print('woof!')
-dog_voice()
 
-#Вывод голосов N раз
+x = input('Введите текст')
 
-def cat_voice ():
-    return ('meow!')
-for x in range(2):
-    print(cat_voice())
+def get_voice(x):
+    if '!' in x:
+        return x
+    else:
+        return False
 
-def dog_voice ():
-    return ('woof!')
-for x in range(2):
-    print(dog_voice())
+print (get_voice(x))
