bb6734421eae8554e034df94b3508628bc1aca10
Vandalism <anotherboxforspam@gmail.com>
2021-08-16 18:19:33 +0300

    catvoice-dogvoice

--- catvoice.py
+++ catvoice.py
@@ -6,4 +6,14 @@ def dog_voice ():
     print('woof!')
 dog_voice()
 
+#Вывод голосов N раз
 
+def cat_voice ():
+    return ('meow!')
+for x in range(2):
+    print(cat_voice())
+
+def dog_voice ():
+    return ('woof!')
+for x in range(2):
+    print(dog_voice())
